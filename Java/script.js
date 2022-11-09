04036db0cdf1e9101560fcb32dd79cae3c042623
Mariel <Mariel@yahoo.com>
2022-11-09 13:52:16 -0800

    for loops ex let item of fruits

--- Java/script.js
+++ Java/script.js
@@ -1,10 +1,45 @@
-for (let i=1; i<=10; i++){
-    num = 2 **i;
-    console.log(num);
+// for (let i=1; i<=10; i++){
+//     num = 2 **i;
+//     console.log(num);
+// }
+// let i=1;
+// while (i<11){
+//     num = 2 **i;
+//     i++;
+//     console.log(num);
+// }
+
+// let numberedInput;
+// let input = "";
+// do{
+// input = prompt("what is your name"); // always return a string
+// console.log(Number (input));
+// numberedInput = Number(input);
+// }
+// while(!isNaN(numberedInput)) //if user enters a number the result of Number() is a number, else it is NaN
+
+// alert("Hello, " + input);
+// alert(`Hello, ${input}`);
+
+
+// let fruits = ['Apple','Banana','Strawberry'];
+
+// let Orange = fruits.push('Orange');
+// console.log(Orange);
+
+// fruits.pop();
+// fruits.unshift('Mango');
+// fruits.shift();
+
+// console.log(fruits);
+
+let fruits = ['Apple','Banana','Strawberry','Mango','Orange'];
+
+
+for (let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
 }
-let i=1;
-while (i<11){
-    num = 2**i;
-    i++;
-    console.log(num);
+
+for (let item of fruits){
+    console.log(item);
 }

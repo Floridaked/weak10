bdbee2f8e48d16de240ced1094fb31c3131ff96e
Mariel <Mariel@yahoo.com>
2022-11-09 14:11:55 -0800

    let in/of practice

--- Java/script.js
+++ Java/script.js
@@ -33,13 +33,57 @@
 
 // console.log(fruits);
 
-let fruits = ['Apple','Banana','Strawberry','Mango','Orange'];
+// let fruits = ['Apple','Banana','Strawberry','Mango','Orange'];
 
 
-for (let i=0;i<fruits.length;i++){
-    console.log(fruits[i]);
+// for (let i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+// }
+
+// for (let item of fruits){
+//     console.log(item);
+// }
+
+// let student1 = {
+//     ID: "A00022",
+//     GPA: 3.0,
+//     program: "CIT"
+// };
+
+// let student2 = {
+//     ID: "A01000",
+//     GPA: 3.1,
+//     program: "CST"
+// };
+
+// let student3 = {
+//     ID: "A00114",
+//     GPA: 3.2,
+//     program: "CIT"
+// };
+// let students = [student1, student2, student3]
+
+// let counter = 0;
+// for (let i = 0; i <students.length;i++){
+// //for (let item of students){
+//     if (item.program === "CIT" && item.GPA>3){
+//         console.log(item);
+//         counter = counter +1;
+//     }
+// }
+// console.log(counter);
+
+const person = {
+    name:['Bob', 'Smith'],
+    age:32,
+    gender:'male',
+    hobbies :['music', 'skiing']
 }
 
-for (let item of fruits){
-    console.log(item);
+for (let key in person){
+    console.log(key);
+    console.log(person[key]);
 }
+
+// console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies.join(' and ')}.  `)
+
